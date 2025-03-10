e66e5f83ea9bc9c644cc19c6d817efa3f2d03809
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2025-03-10 09:43:24 -0300

    fix title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $title ?? 'Page Title' }}</title>
+    <title>{{ $title ?? 'Dream Cars' }}</title>
 
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
